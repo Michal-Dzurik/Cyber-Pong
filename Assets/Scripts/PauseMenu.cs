8a0a6b7d1f58f078e3e52a4a11270782f1670c6d
Michal-Dzurik <misko7104@gmail.com>
2022-03-24 08:44:38 +0100

    Ball layering fixed

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -6,13 +6,18 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     // Start is called before the first frame update7
+    [SerializeField] GameObject ball;
     [SerializeField] GameObject pauseMenu;
     private bool pauseMenuVisible = false;
     public CursorControler cursorControler;
     public ChangeScene sceneManager;
+    
+    private Color transparent;
+    private Color white;
 
     void Start(){
-    
+        transparent = new Color(0,0,0,0f);
+        white = new Color(1f,1f,1f,1f);
     }
 
     void Update(){
@@ -31,14 +36,17 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 0f;
         pauseMenuVisible = true;
         cursorControler.Show();
+        ball.GetComponent<SpriteRenderer>().color = transparent;
     }
 
     public void Resume()
     {
+        ball.GetComponent<SpriteRenderer>().color = white;
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
         pauseMenuVisible = false;
         cursorControler.Hide();
+        
     }
 
     public void MainMenu(){
